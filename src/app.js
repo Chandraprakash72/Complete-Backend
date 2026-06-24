a1219a1f84a496052f7d40952ddadf6935c9937d
Chandra Prakash <hsv7238@gmail.com>
2026-06-24 11:47:37 +0530

    controllers and debugging

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors";
-import cookiesParser from "cookies-parser"
+import cookieParser from "cookie-parser";
 
 
 const app = express()
@@ -14,9 +14,13 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
 
+app.use(cookieParser())
+//routes import
+import userRouter from './routes/user.routes.js'
 
 
-app.use(cookiesParser())
+//routes decleration
+app.use("/api/v1/users", userRouter)
 
-
+// http://localhost:8000/api/v1/users/register
 export{app}
